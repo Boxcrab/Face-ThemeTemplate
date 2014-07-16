172fee1860c199468ce175fb4e2ebf0e25b40d45
Stephen Bird <bird@stephens-mbp.ooo>
2014-07-15 22:47:49 -0700

    initial template design

---
+++ app/src/main/java/skylimit/themetemplate/MainActivity.java
@@ -0,0 +1,16 @@
+package skylimit.themetemplate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by bird on 7/15/14.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
