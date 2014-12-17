91d069a0f4258f2110b6e28f5ff512561030b499
Stephen Bird <sebirdman@gmail.com>
2014-12-16 21:11:24 -0800

    Update to new build tools and fix hide icon bug

---
+++ app/src/main/java/skylimit/themetemplate/BlankActivity.java
@@ -0,0 +1,12 @@
+package skylimit.themetemplate;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class BlankActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
